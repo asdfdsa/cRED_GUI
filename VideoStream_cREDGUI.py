fdd62913f1b1cd15275d68546494826e25b6e07e
Firefox12138 <wangbin911217@sina.com>
2017-09-15 09:57:37 +0200

    GUI image display range, changed from brightness

--- VideoStream_cREDGUI.py
+++ VideoStream_cREDGUI.py
@@ -86,7 +86,7 @@ class VideoStream(threading.Thread):
         self.frame_delay = 50
 
         self.frametime = 0.05
-        self.brightness = 1.0
+        self.brightness = 11800
 
         self.last = time.time()
         self.nframes = 1
@@ -170,9 +170,9 @@ class VideoStream(threading.Thread):
         Label(frame, anchor=E, width=lwidth, text="exposure (s)").grid(row=1, column=0)
         self.e_frametime.grid(row=1, column=1)
 
-        self.e_brightness = Spinbox(frame, width=ewidth, textvariable=self.var_brightness, from_=0.0, to=10.0, increment=0.1)
+        self.e_brightness = Spinbox(frame, width=ewidth, textvariable=self.var_brightness, from_=0.0, to=11800, increment=10)
         
-        Label(frame, anchor=E, width=lwidth, text="Brightness").grid(row=1, column=2)
+        Label(frame, anchor=E, width=lwidth, text="Max Intensity").grid(row=1, column=2)
         self.e_brightness.grid(row=1, column=3)
         
         frame.pack()
@@ -203,7 +203,7 @@ class VideoStream(threading.Thread):
         self.var_frametime.set(self.frametime)
         self.var_frametime.trace("w", self.update_frametime)
 
-        self.var_brightness = DoubleVar(value=1.0)
+        self.var_brightness = DoubleVar(value=11800)
         self.var_brightness.set(self.brightness)
         self.var_brightness.trace("w", self.update_brightness)
 
@@ -260,10 +260,13 @@ class VideoStream(threading.Thread):
 
         frame = np.rot90(frame, k=3)
 
-        if self.brightness != 1:
-            image = Image.fromarray(frame).convert("L")
-            image = ImageEnhance.Brightness(image).enhance(self.brightness)
+        if self.brightness != 11800:
+            #image = Image.fromarray(frame).convert("L")
+            #image = ImageEnhance.Brightness(image).enhance(self.brightness)
             # Can also use ImageEnhance.Sharpness or ImageEnhance.Contrast if needed
+            image=np.clip(frame,0,self.brightness)
+            image=image/self.brightness*11800
+            image=Image.fromarray(image)
 
         else:
             image = Image.fromarray(frame)
